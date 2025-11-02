c5fe8cd30db3e5195222946eea2bc35393932048
Chris Couzens <ccouzens@gmail.com>
2025-11-02 19:20:22 +0000

    Add some basic colour support
    So we can tell the fingers apart

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ if (!(main instanceof SVGGraphicsElement)) {
 	throw new Error("main not instance of SVGElement");
 }
 
+const colours = [
+	"white",
+	"green",
+	"purple",
+	"yellow",
+	"blue",
+	"orange",
+	"pink",
+	"grey",
+	"black",
+];
+
 const m = main.getScreenCTM();
 if (m === null) {
 	throw new Error("getScreenCTM not expected to be null");
@@ -24,6 +36,7 @@ const touchHandler: (e: TouchEvent) => void = (e) => {
 		svgCircle.setAttribute("r", 0.1);
 		svgCircle.setAttribute("cx", svgX);
 		svgCircle.setAttribute("cy", svgY);
+		svgCircle.setAttribute("fill", colours[identifier] ?? "yellow");
 		main.append(svgCircle);
 		console.log(identifier);
 	}
